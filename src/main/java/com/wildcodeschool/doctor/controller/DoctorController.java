c64e28abc1f9bc4a9682b4bf679c82e9d43d4cd1
godeline <godelineboy@gmail.com>
2021-12-22 12:21:51 +0100

    challenge spring routes

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,33 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
+    	
+    	//retourne le Json ...
+    	if (number == 13) {
+    		return new Doctor(13, "Jodie Whittaker");
+    	
+    	// si doc 1 à 12 retourne status 303
+    	}else if (number >= 1 && number <= 12) {
+    		throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+    	
+    	// si doc invalise retourne status 404
+    	}else {
+    		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossiblede récupérer l'incarnation" + number);
+    	}
 
-        return null;
     }
 }
